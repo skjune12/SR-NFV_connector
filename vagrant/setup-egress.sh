5c336536fe2f46bdbbbf9b2bbbf0d0973bb69969
Ahmed AbdelSalam <amsalam20@gmail.com>
2017-01-27 17:11:44 +0100

    Update setup-egress.sh

--- vagrant/setup-egress.sh
+++ vagrant/setup-egress.sh
@@ -6,9 +6,9 @@ ifconfig eth1 up
 ip -6 addr add CCCC::2/64 dev eth1
 
 # segment routing encapsulation of egress node
-ip -6 route add AAAA::2/128 via CCCC::1 encap seg6 mode encap segs BBBB::2,AAAA::2
+#ip -6 route add AAAA::2/128 via CCCC::1 encap seg6 mode encap segs BBBB::2,AAAA::2
 
 # static routing configuration of ingress node
 ip -6 route add BBBB::/64 via CCCC::1
-
+ip -6 route add AAAA::/64 via CCCC::1
 exit

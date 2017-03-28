9ff9356f88da4995772caf33177a0078ae5127fa
Ahmed AbdelSalam <amsalam20@gmail.com>
2017-03-28 11:46:18 +0200

    Update IP6  addresses
    Change IP6 addresses in order to make them simpler

--- vagrant/setup-ingress.sh
+++ vagrant/setup-ingress.sh
@@ -2,12 +2,12 @@
 
 #configuration of ingress node interfaces 
 ifconfig eth1 up
-ip -6 addr add AAAA::2/64 dev eth1
+ip -6 addr add A::2/64 dev eth1
 
 # segment routing encapsulation of ingress node
-ip -6 route add cccc::2/128 via aaaa::1 encap seg6 mode encap segs BBBB::2,CCCC::2
+ip -6 route add C::2/128 via A::1 encap seg6 mode encap segs B::2,C::2
 
 # static routing configuration of ingress node
-ip -6 route add BBBB::/64 via AAAA::1
+ip -6 route add B::/64 via A::1
 
 exit
